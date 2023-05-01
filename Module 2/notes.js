9d6204b25f32b8573b837a194979f1573523fc4c
Arman-Kabir <armankabir8@gmail.com>
2023-05-01 19:26:52 +0600

    nvm installed

---
+++ Module 2/notes.js
@@ -0,0 +1,14 @@
+/*
+    #1-Installation typescript , nvm & ts-node-dev
+    #NVM
+        nvm --v
+        nvm list
+        nvm install 18.16.0
+        nvm use 18.16.0
+
+
+    #TypeScript
+        npm install -g typescript
+        tsc --v
+        tsc is typescript compiler that converts the typescript to javascript
+*/
