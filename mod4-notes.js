88638c0617112d34473d3bd40eb4529e7e43dd89
Arman-Kabir <armankabir8@gmail.com>
2023-05-05 00:39:31 +0600

    oop started

---
+++ mod4-notes.js
@@ -0,0 +1,6 @@
+/*
+### 2: How to Create Class, Object , Parameter Properties
+    ---------------------------------------------------
+    this property doesn't work in arrow function, so we don't use arrow function in class method
+
+*/
---
+++ src/module4/2-object.ts
@@ -0,0 +1,13 @@
+class Animal{
+    name:string;
+    species:string;
+    sound:string;
+
+    constructor(name:string,species:string,sound:string){
+        this.name = name
+        this.species = species
+        this.sound = sound
+    }
+
+    
+}
