65386711497143887f4d759884f52872ca7cf984
Arman-Kabir <armankabir8@gmail.com>
2023-05-04 23:49:51 +0600

    baki ase onek

--- src/module3/5-generic-function.ts
+++ src/module3/5-generic-function.ts
@@ -1,17 +1,17 @@
-// Arrow Function
+// // Arrow Function
 
-const createArray = (param:string ): string[] => {
-  return [param];
-};
+// const createArray = (param:string ): string[] => {
+//   return [param];
+// };
 
-const createArray1 = <X,Y>(param1: X,param2:Y): [X,Y] => {
-  return [param];
-};
+// const createArray1 = <X,Y>(param1: X,param2:Y): [X,Y] => {
+//   return [param];
+// };
 
-const result1 = createArray1<string>('Bangladesh');
-const result2 = createArray1<boolean>(false);
+// const result1 = createArray1<string>('Bangladesh');
+// const result2 = createArray1<boolean>(false);
 
-type Name = {name:string};
+// type Name = {name:string};
 
 
-const result3 = createArray1<Name>({name:"Bangladesh"});
+// const result3 = createArray1<Name>({name:"Bangladesh"});
