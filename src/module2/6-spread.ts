bf0b2ed218fb495bc8aeff458560c3761ae388f5
Arman-Kabir <armankabir8@gmail.com>
2023-05-09 12:21:35 +0600

    m2-6 starts

---
+++ src/module2/6-spread.ts
@@ -0,0 +1,27 @@
+// Js uses 2 types of functions -- normal function,arrow function..
+// normal function
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log(add(2, 2));
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+const arr = [1, 4, 5];
+const newArray = arr.map((elem: number) => elem * elem);
+console.log(newArray);
+
+const person: {
+  name: string;
+  balance: number;
+  addBalance(money: number): void;
+} = {
+  name: "Mezba",
+  balance: 5,
+  addBalance(money: number) {
+    console.log(`my new Balance is ${this.balance + money}`);
+  },
+};
+
+person.addBalance(15);
