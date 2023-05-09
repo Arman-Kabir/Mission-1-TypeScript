531c30071b7088c90f63ffb4f65260b68d8ed363
Arman-Kabir <armankabir8@gmail.com>
2023-05-09 11:35:12 +0600

    m2-3 done

---
+++ src/module2/3-array-tuple.ts
@@ -0,0 +1,29 @@
+// const names:string[] = ["abul", "kabul", "babul"];
+
+// const names = ["abul", "kabul", "babul",77,true];
+// names.push(null);
+
+// const rollNumbers:number[] = [55,4,88];
+// rollNumbers[8] = 99;
+
+// console.log(names);
+// console.log(rollNumbers);
+// rollNumbers.map(roll=>roll.)
+// names.map(name=>name.)
+
+// const user:[number,string] = [412,'Mezba']; // 
+// user[0] = 'gg';
+
+// const boyFriends : [string,boolean] = ['abul',false];
+// boyFriends[1]= true;
+// console.log(user);
+// console.log(boyFriends);
+
+
+
+
+
+// implicit type is applied here in ts.
+// we could declare explicit type -- : string[]
+
+// const user:[number,string] = [412,'Mezba'];
