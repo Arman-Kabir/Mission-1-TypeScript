a04eeb7006decaa3fb6030a50b7277adedd106bf
Arman-Kabir <armankabir8@gmail.com>
2023-05-10 21:04:46 +0600

    rest using string done

--- src/co/5spread-rest.ts
+++ src/co/5spread-rest.ts
@@ -3,15 +3,21 @@ const heroArr1: string[] = ["Khan", "jalil", "siam"];
 const heroArr2: string[] = ["alom", "faru", "raz"];
 
 heroArr1.push(...heroArr2);
-console.log(heroArr1);
+// console.log(heroArr1);
 
 //rest
 const heroScore = (...score: number[]): number => {
   let sum: number = 0;
   score.map((n) => {
-    sum = sum+n;
+    sum = sum + n;
   });
 
   return sum;
 };
-console.log(heroScore(1, 4, 5,  8, 9));
+// console.log(heroScore(1, 4, 5,  8, 9));
+
+// rest using string
+const heroString = (...strings: string[]): void => {
+  strings.map((str) => console.log(str)); 
+};
+console.log(heroString("programming", "hero", "apollo", "web"));
