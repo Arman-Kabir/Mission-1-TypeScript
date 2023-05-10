03313b36787805575d397db8c62d67085c1d669e
Arman-Kabir <armankabir8@gmail.com>
2023-05-10 20:22:02 +0600

    arrow func

--- src/co/4func.ts
+++ src/co/4func.ts
@@ -1,7 +1,14 @@
 // normal func
-function heroNormalFunc(a:number, b:number):number {
+function heroNormalFunc(a: number, b: number): number {
   const sum = a + b;
   return sum;
 }
 
-console.log(heroNormalFunc(2,3));
+// console.log(heroNormalFunc(2,3));
+
+// arrow func
+const heroArrowFunc = (c:number, d:number):string => {
+  return `${c + d}`;
+};
+
+console.log(heroArrowFunc(5,6));
