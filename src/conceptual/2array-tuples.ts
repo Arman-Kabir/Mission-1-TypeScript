8ba3a38ef9c7646c2319b1834ea87b5bfb7de544
Arman-Kabir <armankabir8@gmail.com>
2023-05-10 19:51:53 +0600

    tuple done

--- src/conceptual/2array-tuples.ts
+++ src/conceptual/2array-tuples.ts
@@ -3,4 +3,10 @@ const heroes: string[] = ["ananta", "bappa"];
 const nums: number[] = [1, 2, 3];
 const heroInfo: (string | number | boolean)[] = ["Khan", "Actor", 32, true];
 
-//tuple
+//tuple - specifying which type of data will be assigned in which index in an array.
+const heroTuple: [number, number, boolean] = [
+  12,
+  34,
+  true,
+  
+];
