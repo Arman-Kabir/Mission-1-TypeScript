c97f4b81a9d10960d63c2bb29e483c1c4c93508b
Arman-Kabir <armankabir8@gmail.com>
2023-05-10 21:23:42 +0600

    type alias

---
+++ src/co/6alias.ts
@@ -0,0 +1,20 @@
+type HeroIncomeType = number | string; // | called union
+const heroIncome: HeroIncomeType = "34";
+
+type HeroType = {
+  name: string;
+  age: number;
+};
+
+const hero4: HeroType = {
+  name: "khan",
+  age: 32,
+};
+const hero5: HeroType = {
+  name: "siam",
+  age: 30,
+};
+const hero6: HeroType = {
+  name: "siam",
+  age: 35,
+};
