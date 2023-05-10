75bf92d869cf8110013313bc4d343bea1e7d37c8
Arman-Kabir <armankabir8@gmail.com>
2023-05-10 20:46:24 +0600

    rest

--- src/co/5spread-rest.ts
+++ src/co/5spread-rest.ts
@@ -2,5 +2,16 @@
 const heroArr1: string[] = ["Khan", "jalil", "siam"];
 const heroArr2: string[] = ["alom", "faru", "raz"];
 
-heroArr1.push('arifin');
+heroArr1.push(...heroArr2);
 console.log(heroArr1);
+
+//rest
+const heroScore = (...score: number[]): number => {
+  let sum: number = 0;
+  score.map((n) => {
+    sum = sum+n;
+  });
+
+  return sum;
+};
+console.log(heroScore(1, 4, 5,  8, 9));
