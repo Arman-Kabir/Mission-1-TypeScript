d58a025dacabf2e19964cc73061922e6a2c13c67
Arman-Kabir <armankabir8@gmail.com>
2023-05-11 23:07:53 +0600

    in guard

--- src/co/14type-guard.ts
+++ src/co/14type-guard.ts
@@ -1,11 +1,31 @@
 //of guard
 type typeOfData = number | string;
 const doSomething = (a: typeOfData): typeOfData => {
-    if(typeof a === 'number'){
-        return a;
-    }
+  if (typeof a === "number") {
     return a;
+  }
+  return a;
 };
 
 console.log(doSomething(23));
-console.log(doSomething('hello'));
+console.log(doSomething("hello"));
+
+//in guard
+type X = {
+  name: string;
+  phone: number;
+};
+
+type Y = {
+  name: string;
+  address: string;
+  salary: number;
+};
+
+const getInfo = (info:X|Y) => {
+    if('phone' in info ){
+        return `Type A:phone: ${info.phone}`;
+    }else if('salary in info'){
+        return `Type B: salary: ${info.salary}`;
+    }
+};
