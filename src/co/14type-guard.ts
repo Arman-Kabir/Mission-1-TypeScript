c6c420ea384bf2737135c477e30f53d180d13b84
Arman-Kabir <armankabir8@gmail.com>
2023-05-12 00:08:46 +0600

    instanceof guard

--- src/co/14type-guard.ts
+++ src/co/14type-guard.ts
@@ -22,10 +22,56 @@ type Y = {
   salary: number;
 };
 
-const getInfo = (info:X|Y) => {
-    if('phone' in info ){
-        return `Type A:phone: ${info.phone}`;
-    }else if('salary in info'){
-        return `Type B: salary: ${info.salary}`;
-    }
+const getInfo = (info: X | Y) => {
+  if ("phone" in info) {
+    return `Type A:phone: ${info.phone}`;
+  } else if ("salary in info") {
+    return `Type B: salary: ${info.salary}`;
+  }
 };
+
+//instance of guard
+class P {
+  position: string;
+  salary: number;
+
+  constructor(position: string, salary: number) {
+    this.position = position;
+    this.salary = salary;
+  }
+
+  showDetails() {
+    console.log(`position:${this.position}`);
+  }
+}
+
+class Q extends P {
+  constructor(position: string, salary: number) {
+    super(position, salary);
+  }
+  showSalary() {
+    console.log(`Salary: ${this.salary},position:${this.position}`);
+  }
+}
+
+class R extends Q {
+  constructor(position: string, salary: number) {
+    super(position, salary);
+  }
+  showSalarywithPosition() {    
+    console.log(`Salary: ${this.salary},position:${this.position}`);
+  }
+}
+
+function findInstance(person:P){
+    if(person instanceof Q){
+        person.showDetails();
+    }else if(person instanceof R){
+        person.showSalarywithPosition();
+    }
+}
+
+
+const p4 = new Q('developer',23);
+const p5 = new R('developer',33);
+
