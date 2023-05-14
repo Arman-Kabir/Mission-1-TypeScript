a4ba36d4848e4df64a62fdf4f3a46331469f0eb6
Arman-Kabir <armankabir8@gmail.com>
2023-05-14 19:00:10 +0600

    generic type

--- src/module3/3-generic-type.ts
+++ src/module3/3-generic-type.ts
@@ -1,12 +1,34 @@
+type GenericTuple<X, Y> = [X, Y];
 
-type GenericArray<T>=Array<T>;
+const relation: GenericTuple<string, string> = ["persian", "kate winslet"];
 
+type RelationWithSalaryType = { name: string; salary: number };
+
+// interface 
+
+const relationWithSalary: GenericTuple<RelationWithSalaryType, string> = [
+  {
+    name: "Persian",
+    salary: 10000,
+  },
+  "ket wins",
+];
+
+const relationWithSalary2: GenericTuple<object, string> = [
+  {
+    name: "Persian",
+    salary: 10000,
+  },
+  "ket wins",
+];
+
+type GenericArray<T> = Array<T>;
 
 const rollNumbers: GenericArray<number> = [44, 12, 4];
 const rollNumbers2: GenericArray<string> = ["44", "12", "4"];
 const rollNumbers3: GenericArray<boolean> = [true, false];
 
-type NameRollType = { name: string; roll: number }
+type NameRollType = { name: string; roll: number };
 
 const userNameAndRollNumbers: GenericArray<NameRollType> = [
   {
