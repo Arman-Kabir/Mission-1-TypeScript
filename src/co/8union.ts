f8badff70a59c2c6b82bfef5388536f7c5847eb0
Arman-Kabir <armankabir8@gmail.com>
2023-05-19 10:24:51 +0600

    union

--- src/co/8union.ts
+++ src/co/8union.ts
@@ -2,18 +2,24 @@
 
 type HeroTypeA = {
   name: string;
-  age: Number;
+  age?: Number;
 };
 
-type HeroTypeB =HeroTypeA {
-  
-  salary:number;
-  position:string;
+type HeroTypeB = HeroTypeA & {
+  salary: number;
+  position?: string;
 };
 
-const HeroData:HeroTypeB {
-    name:"jalil",
-    age:23,
-    salary:3534,
-    position:"Developer"
-}
+const HeroData: HeroTypeB = {
+  name: "jalil",
+  age: 23,
+  salary: 35624,
+  position: "Developer",
+};
+
+const myFavHero: HeroTypeA | HeroTypeB = {
+  name: "Khan",
+  age: 30,
+  salary: 34454,
+  position: "Actor",
+};
