5dce7040d4f39d29998e9aca75d10ecc05731a0b
Arman-Kabir <armankabir8@gmail.com>
2023-05-19 11:03:15 +0600

    ternary

---
+++ src/co/10ternary.ts
@@ -0,0 +1,11 @@
+const isHeroAvailable: boolean = false;
+
+if (isHeroAvailable) {
+  console.log("book now");
+} else {
+  console.log("u can't book");
+}
+
+const checkAvialability = isHeroAvailable ? "book now" : " can't book";
+
+console.log(checkAvialability);
