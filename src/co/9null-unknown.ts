09664771292e12433ea63bd7c4f32e8e9092fb96
Arman-Kabir <armankabir8@gmail.com>
2023-05-19 10:46:46 +0600

    null-unknown

--- src/co/9null-unknown.ts
+++ src/co/9null-unknown.ts
@@ -1,1 +1,20 @@
 //null
+const myText = (text: string | null): void => {
+  if (text === null) {
+    console.log("I have no text");
+  } else {
+    console.log(text);
+  }
+};
+
+console.log(myText(null));
+console.log(myText("This is not a null value"));
+
+const guessSomething = (myGuess: unknown) => {
+  console.log(myGuess);
+};
+console.log(guessSomething(23));
+console.log(guessSomething("Hello"));
+console.log(guessSomething(null));
+console.log(guessSomething(undefined));
+console.log(guessSomething(true));  
